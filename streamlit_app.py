b12aabb6dd4b92768ee1fd060617b33d9354b629
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-06 19:30:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,13 @@ st.sidebar.image(logo)
 # Customize page title
 st.title("Streamlit for Geospatial Applications")
 
-st.markdown(
-    """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
-    """
-)
+# st.markdown(
+#     """
+#     This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+#     """
+# )
 
-st.header("Instructions")
+#st.header("Instructions")
 
 markdown = """
 1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
@@ -33,7 +33,7 @@ markdown = """
 
 """
 
-st.markdown(markdown)
+#st.markdown(markdown)
 
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
