a89b1a5188b19345dc8b8b2712e72a9fed817b2d
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-06 19:57:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,11 @@ markdown = """
 #st.markdown(markdown)
 
 #m = leafmap.Map(minimap_control=True)
-m = leafmap.Map(center=(15.8700, 100.9925), zoom=4, height="400px", width="300px")
+m = leafmap.Map(center=(15.8700, 100.9925), zoom=4, height="200px", width="100px",
+                draw_control=False,
+                measure_control=False,
+                fullscreen_control=True,
+               )
 
 #m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=700)
