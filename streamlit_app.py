7baa6e574c08bfdc1fe13db81174205f14209f81
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-07 17:13:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,14 @@ GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
 # Customize page title
 st.title("Air Quality Map")
 
+components.html(
+        """
+        <div id="openweathermap-widget-11"></div>
+        <script src='//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'></script>
+        <script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 11,cityid: '2643743',appid: '92950a35fe2d01aae4d472fe6fc956a1',units: 'metric',containerid: 'openweathermap-widget-11',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
+        """
+    )
+
 df2 = pd.read_csv('th.csv')
 #st.write(len(df2))
 df3 = df2[0:50]
