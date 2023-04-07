83ba7ed42b351bf91cc45ebf8e902e3fcd124d94
Seubson Soisuvarn <seubsons@gmail.com>
2023-04-07 03:34:09 -0400

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ def getdata(lat, lon):
 
 df2 = df2.assign(pm2_5=[0] * len(df2))
 for c in np.arange(len(df2)):
-    st.write(c)
+    #st.write(c)
     pm2_5 = getdata(df2.loc[c, 'lat'], df2.loc[c, 'lng'])
     df2.loc[c, 'pm2_5'] = pm2_5
 st.write(df2)
