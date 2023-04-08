64335553b496adb8773576ccee617f3a50ee71dd
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-08 10:50:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,12 @@
 import streamlit as st
-from leafmap import folium
-
+from streamlit_folium import folium_static
+from leafmap import folium as leaf_folium
 
 api_key = st.secrets["pass"]
-
 
 st.set_page_config(layout="wide")
 
 st.title("Air Quality Map")
 
-
-m = folium.Map(location=[60, 50], zoom_start=3)
-m.to_streamlit(height=700)
-
+m = leaf_folium.Map(location=[60, 50], zoom_start=3)
+folium_static(m, height=700)
