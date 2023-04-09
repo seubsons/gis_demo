1df2f2e658c4617a7eec7e16176665861d5859a1
Seubson Soisuvarn <seubsons@gmail.com>
2023-04-08 20:55:18 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ df3 = df3.assign(pm2_5=[0] * len(df3))
 for c in np.arange(len(df3)):
     pm2_5, data = getdata(df3.loc[c, 'lat'], df3.loc[c, 'lng'])
     df3.loc[c, 'pm2_5'] = pm2_5
-df3.to_csv('owm_pm2_5.csv', index=False)
+#df3.to_csv('owm_pm2_5.csv', index=False)
 
 timestamp = data['list'][0]['dt']
 dt_object = datetime.datetime.fromtimestamp(timestamp)
@@ -64,41 +64,48 @@ with col1:
                 draw_control=False,
                 measure_control=False,
                )
-    m.add_heatmap(
-                df3,
-                latitude="lat",
-                longitude="lng",
-                value="population",
-                name="Heatmap",
-                radius=25)
-    m.to_streamlit(height=700)
-
-with col2:
-    m = leafmap.Map(center=map_center, zoom=6,
-                draw_control=False,
-                measure_control=False,
-               )
-    lat = df3.loc[0, 'lat']
-    lon = df3.loc[0, 'lng']
-    mag = df3.loc[0, 'population']
-    m.add_marker(location=[lat, lon], tooltip=str(mag), icon=Icon(color='red'))
     # m.add_heatmap(
     #             df3,
     #             latitude="lat",
     #             longitude="lng",
-    #             value="pm2_5",
+    #             value="population",
     #             name="Heatmap",
     #             radius=25)
+    for i, row in df3.iterrows():
+        mag = row['pm2_5']
+        lat = row['lat']
+        lon = row['lng']
+        color = 'purple' if mag > 75 else 'red' if mag > 50 else 'orange' if mag > 25 else 'yellow' if mag >10 else 'green'
+        m.add_marker(location=[lat, lon], tooltip=str(mag), icon=Icon(color=color))
+
     m.to_streamlit(height=700)
 
 # with col2:
-#     show_temp = st.beta_expander(label='PM2.5')
-#     df4 = df3.copy()
-#     df4['population'] = df4['population'].apply('{:,.0f}'.format)
-#     df4['pm2_5'] = df4['pm2_5'].apply('{:.2f}'.format)
+#     m = leafmap.Map(center=map_center, zoom=6,
+#                 draw_control=False,
+#                 measure_control=False,
+#                )
+#     lat = df3.loc[0, 'lat']
+#     lon = df3.loc[0, 'lng']
+#     mag = df3.loc[0, 'population']
+#     m.add_marker(location=[lat, lon], tooltip=str(mag), icon=Icon(color='red'))
+#     # m.add_heatmap(
+#     #             df3,
+#     #             latitude="lat",
+#     #             longitude="lng",
+#     #             value="pm2_5",
+#     #             name="Heatmap",
+#     #             radius=25)
+#     m.to_streamlit(height=700)
 
-#     with show_temp:
-#         st.table(df4[['city', 'population', 'pm2_5']])
+with col2:
+    show_temp = st.beta_expander(label='PM2.5')
+    df4 = df3.copy()
+    df4['population'] = df4['population'].apply('{:,.0f}'.format)
+    df4['pm2_5'] = df4['pm2_5'].apply('{:.2f}'.format)
+
+    with show_temp:
+        st.table(df4[['city', 'population', 'pm2_5']])
 
 # //////////////////////////////////////
 st.header("Weather")
